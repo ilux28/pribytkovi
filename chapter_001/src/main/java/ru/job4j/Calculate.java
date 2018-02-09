5b241b6b310ef3a88fb091d14ee83c9b9ff23ad1
ilux28 <ilya.pribytko@mail.ru>
2018-02-09 17:58:44 +0500

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
-
+/**
+ * Class для вычисления арифметических операций + - * / ^.
+ * @author ipribytkov
+ * @since 09.07.2018
+ * @version 1.0
+ */
 public class Calculate {
+    /**
+     * Конструктор, вывод строки в консоль.
+     * @param args - args.
+     */
     public static void main(String[] args){
         System.out.println("Hello world!");
     }
