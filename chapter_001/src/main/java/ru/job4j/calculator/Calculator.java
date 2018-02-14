7e299dd7a869c61a18b0b8f0e0a1dbe81e2be2c2
ilux28 <ilya.pribytko@mail.ru>
2018-02-14 22:36:48 +0500

    2.3. Элементарный калькулятор.

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -7,6 +7,18 @@ public class Calculator {
         this.result = first + second;
     }
 
+    public void substract(double first, double second) {
+        this.result = first - second;
+    }
+
+    public void multiple(double first, double second) {
+        this.result = first * second;
+    }
+
+    public void div(double first, double second) {
+        this.result = first / second;
+    }
+
     public double getResult() {
         return this.result;
     }
--- chapter_001/src/test/java/ru/job4j/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculatorTest.java
@@ -15,6 +15,32 @@ public class CalculatorTest {
         double result = calc.getResult();
         double expected = 2D;
         assertThat(result, is(expected));
+    }
 
+    @Test
+    public void substractTest() {
+        Calculator calc = new Calculator();
+        calc.substract(34, 15);
+        double result = calc.getResult();
+        double expected = 19;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void multipleTest() {
+        Calculator calc = new Calculator();
+        calc.multiple(12, 3);
+        double result = calc.getResult();
+        double expected = 36;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void divTest() {
+        Calculator calc = new Calculator();
+        calc.div(36, 3);
+        double result = calc.getResult();
+        double expected = 12;
+        assertThat(result, is(expected));
     }
 }
