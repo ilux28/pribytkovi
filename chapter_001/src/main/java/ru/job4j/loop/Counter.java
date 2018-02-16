099a619a0e1db4c570c2ee12574a8a071fa7ba13
ilux28 <ilya.pribytko@mail.ru>
2018-02-17 00:19:58 +0500

    2.3. Вычисление площади треугольника

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Counter {
+    public int add(int start, int finish) {
+        int count = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+            count = count + i;
+            }
+        }
+        return count;
+    }
+}
