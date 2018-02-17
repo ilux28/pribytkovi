d49b17c5cb5cd3baa7c61405084b3b06ca3e9298
ilux28 <ilya.pribytko@mail.ru>
2018-02-17 14:43:11 +0500

    4.3. Построение шахматной доски

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Board {
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (int i = 0; i <= height - 1; i++) {
+            for (int j = 0; j <= width - 1; j++) {
+                if ((i + j) % 2 == 0) {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import ru.job4j.loop.Board;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Ilia Pribytkov (ilya.pribytko@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class BoardTest {
+    @Test
+    public void when3x3() {
+        Board board = new Board();
+        String rsl = board.paint(3, 3);
+        String ln = System.lineSeparator();
+        assertThat(rsl, is(String.format("X X%s X %sX X%s", ln, ln, ln)));
+    }
+}
