e65c2cc73eba06e429e58a8c21efb16fad8fd27e
ilux28 <ilya.pribytko@mail.ru>
2018-02-22 21:08:31 +0500

    Сделать из двух отсортированных массивов один отсортированный
    (cherry picked from commit 407da4e97af195f85b55703deebd3749514b5b1f)

--- chapter_001/src/main/java/ru/job4j/array/TwoSortMass.java
+++ chapter_001/src/main/java/ru/job4j/array/TwoSortMass.java
@@ -6,27 +6,18 @@ package ru.job4j.array;
 public class TwoSortMass {
     public int[] rezVozr(int[] first, int[] second) {
         int[] result = new int[first.length + second.length];
-        int[] s;
-        int[] t;
-        if (first.length > second.length) {
-            s = second;
-            t = first;
-        } else {
-            s = first;
-            t = second;
-        }
-        for (int i = 0; i < s.length; i++) {
-            if (first[i] > second[i]) {
-                result[2 * i] = first[i];
-                result[2 * i + 1] = second[i];
+        int j = 0, k = 0;
+        for (int i = 0; i < result.length; i++) {
+
+            if (first[j] < second[k]) {
+                result[i] = first[j];
+                if (j < first.length) j++;
             } else {
-                result[2 * i] = second[i];
-                result[2 * i + 1] = first[i];
+                result[i] = second[k];
+                if (k < second.length) k++;
             }
         } return result;
 
-         for (int j = 0; j < t.length - s.length; j++)  {
-            result[2 * s.length + j] = t[s.length + j];
-        } return result;
+
     }
 }
