9b19ff4fd49cfe1f388a1185e16600809a117291
ilux28 <ilya.pribytko@mail.ru>
2018-02-22 21:20:43 +0500

    Сделать из двух отсортированных массивов один отсортированный

---
+++ chapter_001/src/test/java/ru/job4j/array/TwoSortMassTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TwoSortMassTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        int[] a = {0, 2, 4};
+        int[] b = {1, 3, 5};
+        TwoSortMass re = new TwoSortMass();
+        int[] result = re.rezVozr(a, b);
+        int[] expended = {0, 1, 2, 3, 4, 5};
+        assertThat(result, is(expended));
+    }
+}
+
+
+
