95d7924f31bd4aab8f366122fbe0b68ba4906fa1
ilux28 <ilya.pribytko@mail.ru>
2018-03-14 14:44:40 +0500

    CofeeMachine 1. Телефонный справочник на базе ArrayList.

---
+++ chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,19 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+    /**
+     * Метод должен вставлять в нужную позицию элемент.
+     * Позиция определять по полю приоритет.
+     * Для вставик использовать add(int index, E value)
+     * @param task задача
+     */
+    public void put(Task task) {
+        this.tasks.addFirst(task);
+    }
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PriorityQueueTest {
+    @Test
+    public void whenHigherePriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("midle", 3));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("midle"));
+    }
+}
