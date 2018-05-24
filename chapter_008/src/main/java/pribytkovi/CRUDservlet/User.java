b3eee2e32460d8d473d22b66ad9fd585a937fe25
ilux28 <ilya.pribytko@mail.ru>
2018-05-24 23:15:24 +0500

    CruidServlet

---
+++ chapter_008/src/main/java/pribytkovi/CRUDservlet/User.java
@@ -0,0 +1,35 @@
+package pribytkovi.CRUDservlet;
+
+import com.sun.jmx.snmp.Timestamp;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private Timestamp createDate;
+
+    public int getId() {
+        return this.id;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public Timestamp getCreateDate() {
+        return this.createDate;
+    }
+    public void setCreateDate(Timestamp createDate) {
+        this.createDate = createDate;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
