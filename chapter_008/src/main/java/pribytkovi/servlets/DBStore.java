0f2d3b6db549253a378fbdebb093fab5591b7255
ilux28 <ilya.pribytko@mail.ru>
2018-06-20 00:14:35 +0500

    JSPServlet

--- chapter_008/src/main/java/pribytkovi/servlets/DBStore.java
+++ chapter_008/src/main/java/pribytkovi/servlets/DBStore.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Vector;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,15 +16,42 @@ public class DBStore implements Store {
     String url;
     String driver;
     int initConnCnt;
+    private static BasicDataSource  dataSource;
+    private static BasicDataSource getDataSource() {
+        if (dataSource == null) {
+            BasicDataSource ds = new BasicDataSource();
+            ds.setUrl("jdbc:postgresql://localhost:5432/contactdb");
+            ds.setUsername("postgres");
+            ds.setPassword("123");
 
-    @Override
-    public void add() {
-    ConnectionPool con = new ConnectionPool(this.url, this.driver, this.initConnCnt);
+            ds.setMinIdle(5);
+            ds.setMaxIdle(10);
+            ds.setMaxOpenPreparedStatements(100);
 
+            dataSource = ds;
+        }
+        return dataSource;
     }
-
     @Override
-    public void update() {
+    public void add(String name, String password) {
+
+        String sql = "INSERT INTO USERS (LOGIN, PASSWORD) VALUES (?, ?)";
+        try (BasicDataSource dataSource = DBStore.getDataSource();
+        Connection connection = dataSource.getConnection();
+        PreparedStatement pstmt = connection.prepareStatement(sql);
+        )
+        {
+            System.out.println("The connection the connection Object is of Class: " + connection.getClass());
+            pstmt.setString(1, name);
+            pstmt.setString(2, password);
+            pstmt.executeUpdate();
+            pstmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void update(int id, String name, String password) {
 
     }
 
--- chapter_008/src/main/java/pribytkovi/servlets/Store.java
+++ chapter_008/src/main/java/pribytkovi/servlets/Store.java
@@ -2,8 +2,8 @@ package pribytkovi.servlets;
 
 public interface Store {
 
-    public void add();
-    public void update();
+    public void add(String name, String password);
+    public void update(int id, String name, String password);
     public User delete();
     public User[] findAll();
     public User findBiId();
