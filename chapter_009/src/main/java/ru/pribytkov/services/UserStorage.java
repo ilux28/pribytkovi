cd6f1309d30fc0785facee8cd78e70069e6bf519
ilux28 <ilya.pribytko@mail.ru>
2018-08-30 22:58:49 +0500

    hibernate

--- chapter_009/src/main/java/ru/pribytkov/services/UserStorage.java
+++ chapter_009/src/main/java/ru/pribytkov/services/UserStorage.java
@@ -8,14 +8,23 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import ru.pribytkov.models.User;
 
+import java.util.List;
+
 
 public class UserStorage {
-    SessionFactory factory = new Configuration()
-            .configure()
-            .buildSessionFactory();
-    Session session = factory.openSession();
-    Transaction transaction = session.beginTransaction();
-    User user = new User();
-    user.setId();
-
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure()
+                .buildSessionFactory();
+        Session session = factory.openSession();
+        session.beginTransaction();
+        List<User> users = session.createQuery("from User").list();
+        for (User user : users)
+            System.out.println(user.getName());
+        session.getTransaction().commit();
+        session.close();
+        factory.close();
+        //User user = new User();
+        //user.setId(18);
+    }
 }
