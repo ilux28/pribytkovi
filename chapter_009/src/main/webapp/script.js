7e77ce162145728316c77dbb9c0236a40112f452
ilux28 <ilya.pribytko@mail.ru>
2018-09-24 23:40:20 +0500

    hibernateServletsJSON

--- chapter_009/src/main/webapp/script.js
+++ chapter_009/src/main/webapp/script.js
@@ -1,14 +1,32 @@
 $(document).ready(function() {
     $.ajax('./add', {
-        method : 'get',
-        complete: function(data) {
-            data = JSON.parse(data);
-            for(var key in data) {
-                console.log("key: " + key + "value: " + data[key]);
+        method: 'get',
+        complete: function (data) {
+            /*
+             data = JSON.parse(data);
+             for(var key in data) {
+                 console.log("key: " + key + "value: " + data[key]);
+                 */
+            var result = "<tr>" +
+                "<td>ID</td>" +
+                "<td>Description</td>" +
+                "<td>Created</td>" +
+                "<td>Done</td>" +
+                "</tr>";
+            var items = JSON.parse(data.responseText);
+            for (var i = 0; i != items.length; ++i) {
+                result += "<tr>" +
+                    "<td>" + items[i].id + "</td>" +
+                    "<td>" + items[i].desc + "</td>" +
+                    "<td>" + items[i].created + "</td>" +
+                    /* "<td>" + items[i].done + "</td>" + */
+                    "</tr>";
             }
-
+            var table = document.getElementById("items");
+            table.innerHTML(result);
         }
-    });
+
+    })
 });
 
 
