0f5a82151242c11ec66b56d006e4a63deaa1023e
ilux28 <ilya.pribytko@mail.ru>
2018-09-27 20:05:49 +0500

    hibernateServletsJSON

--- chapter_009/src/main/webapp/script.js
+++ chapter_009/src/main/webapp/script.js
@@ -2,7 +2,25 @@ $(document).ready(function() {
     $.ajax('./add', {
         method: 'get',
         complete: function (data) {
-             data = JSON.parse(data);
+            var respJson = data.responseJSON;
+            console.log(respJson);
+            var resp = data.responseText;
+            console.log(resp);
+            var respJsonParse = JSON.parse(resp);
+            console.log(respJsonParse);
+            var JSONArray = data.array.data;
+            for (var i in JSONArray) {
+                console.log(JSONArray.toString());
+                console.log(JSONArray.valueOf());
+            }
+
+            /*
+            console.log(respJson.desc);
+            console.log(respJson.created);
+            console.log(respJson.done);
+
+
+             //data = JSON.parse(data);
              /* for(var key in data) {
                  console.log("key: " + key + "value: " + data[key]);
              */
