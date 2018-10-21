2d00a2171a1bcb1e613b673b2314e65b340d7c2e
ilux28 <ilya.pribytko@mail.ru>
2018-10-21 10:08:00 +0500

    hibernateServletsJSON

--- chapter_009/src/main/webapp/script.js
+++ chapter_009/src/main/webapp/script.js
@@ -1,8 +1,8 @@
-$(document).ready(complete());
+$(document).ready( complete());
 function complete() {
     $.ajax('./add', {
         method: 'get',
-        complete: function (data) {
+        success: function (data) {
             var table = document.getElementById("items");
             var items = JSON.parse(data.responseText);
             console.log(items);
@@ -19,18 +19,11 @@ function complete() {
                 table.appendChild(newI);
             }
         }
-    });
+    })
 }
-$('#myButton').click($.ajax('./add', {
-    method: 'post',
-    data: {
-        //id: document.getElementsByName("inputDescription").value,
-        description : document.getElementById('inputDescription').value,
-        created: document.getElementById('created').checked,
-        done:  document.getElementById('done').checked
-    }
-}), complete() )
-/*function sendAjaxItem() {
+
+$('#myButton').click(sendAjaxItem(), complete());
+function sendAjaxItem() {
     $.ajax('./add', {
         method: 'post',
         data: {
@@ -42,4 +35,3 @@ $('#myButton').click($.ajax('./add', {
     });
     return false;
 }
-*/
