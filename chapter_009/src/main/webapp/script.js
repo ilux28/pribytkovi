e2a318d58d1f24a5111f82248a775b919665d7f3
ilux28 <ilya.pribytko@mail.ru>
2018-10-28 11:26:00 +0500

    hibernateServletsJSON

--- chapter_009/src/main/webapp/script.js
+++ chapter_009/src/main/webapp/script.js
@@ -1,6 +1,28 @@
-$(document).ready(getData());
-
-
+$(document).ready(getData(), function() {
+ var el = document.getElementsByClassName('menu-item');
+ for (var i = 0; i<el.length; i++) {
+     el[i].addEventListener("mouseenter", showSub, false);
+     el[i].addEventListener("mouseleave", hideSub, false);
+ }
+});
+function showSub(e) {
+    if (this.children.length>1) {
+        this.cildren[1].style.height = "auto";
+        this.cildren[1].style.overflow = "visible";
+        this.cildren[1].style.opacity = "1";
+    } else {
+        return false;
+    }
+}
+function hideSub(e) {
+    if (this.children.length>1) {
+        this.cildren[1].style.height = "0px";
+        this.cildren[1].style.overflow = "hidden";
+        this.cildren[1].style.opacity = "0";
+    } else {
+        return false;
+    }
+}
 function sendData() {
     $.ajax('./add', {
         method: 'post',
